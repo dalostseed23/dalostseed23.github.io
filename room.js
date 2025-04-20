7091d96d69797d5c19e911634cca64d0d9b02603
dalostseed23 <delpinosebastian2004@outlook.com>
2025-04-20 17:23:53 -0400

    finalized basic scene with shadows

--- room.js
+++ room.js
@@ -9,18 +9,35 @@ function main(){
     var container = document.createElement("div");
 	document.body.appendChild(container);
 	renderer = new THREE.WebGLRenderer();
-
 	renderer.setClearColor(new THREE.Color(0x00000000));
 	renderer.setSize(window.innerWidth, window.innerHeight);
 	container.appendChild(renderer.domElement);
 
     scene = new THREE.Scene();
+    const axesHelper = new THREE.AxesHelper( 5 );
+    scene.add( axesHelper );
     initCamera();
     initLighting()
     roomLoader().then(room => {
-        scene.add(room);
         room.position.set(0, 0, 0); 
+        room.castShadow = true;
+        room.receiveShadow = true;
+
+        room.traverse((child) => {
+            if (child.isMesh) {
+              child.castShadow = true;
+              child.receiveShadow = true;
+            }
+          });
+        scene.add(room);
     })
+
+    renderer.shadowMap.enabled = true;
+    renderer.shadowMapSoft = true;
+    renderer.shadowMapBias = 0.0039;
+    renderer.shadowMapDarkness = 0.4; 
+    renderer.shadowMapWidth = 1024;
+    renderer.shadowMapHeight = 1024;
     controls = new OrbitControls(camera, renderer.domElement)
     render();
 }
@@ -28,14 +45,40 @@ function main(){
 function initCamera(){
     var aspect = window.innerWidth / window.innerHeight;
     camera = new THREE.PerspectiveCamera(60, aspect, 1, 1000);
-    camera.position.set(1,0,0)
+    camera.position.set(6, 2, 2);
     camera.lookAt(scene.position);
 }
 
 function initLighting() {
-  // Ambient light
-  const ambientLight = new THREE.AmbientLight(0xffffff, 1.0)
-  scene.add(ambientLight)
+    const pointLight = new THREE.PointLight(0xefc070, 1, 500 );
+    pointLight.position.set(0.75,0.75,1.25);
+    pointLight.castShadow = true; 
+    pointLight.shadow.cameraNear = 0.1;
+    pointLight.shadow.cameraFar = 20; 
+    pointLight.shadow.bias = -0.002;
+    pointLight.shadow.mapSize.width = 1024; 
+    pointLight.shadow.mapSize.height = 1024; // Reduce resolution
+    scene.add(pointLight);
+
+    const directionalLight = new THREE.DirectionalLight(0x506886, 3);
+    directionalLight.position.set(3, 2, -4);
+    directionalLight.target.position.set(0.75,0.75,1.25);
+    directionalLight.castShadow = true; 
+    directionalLight.shadow.cameraNear = 0.1;
+    directionalLight.shadow.cameraFar = 20;
+    directionalLight.shadow.bias = -0.002;
+    directionalLight.shadow.mapSize.width = 1024;
+    directionalLight.shadow.mapSize.height = 1024;
+    
+    scene.add(directionalLight);
+
+    const pointLightColor = pointLight.color;
+    const directionalLightColor = directionalLight.color;
+    const ambientLightColor = new THREE.Color();
+    ambientLightColor.lerpColors(pointLightColor, directionalLightColor, 0.5);
+    const ambientLight = new THREE.AmbientLight(ambientLightColor, 0.15);
+    //scene.add(ambientLight)
+  
 }
 
 function render() {
