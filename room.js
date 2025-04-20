f961aaebb97c3b8689af43a2f726b9ac057bb434
dalostseed23 <delpinosebastian2004@outlook.com>
2025-04-20 14:35:54 -0400

    scene loaded

--- room.js
+++ room.js
@@ -1,39 +1,56 @@
 import * as THREE from 'three';
-let camera, renderer;
+import { roomLoader } from './roomLoader.js';
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
 
-function init(){
+let scene, camera, renderer, controls
+
+function main(){
     
     var container = document.createElement("div");
 	document.body.appendChild(container);
 	renderer = new THREE.WebGLRenderer();
 
-	renderer.setClearColor(new THREE.Color(0x99c0e3));
+	renderer.setClearColor(new THREE.Color(0x00000000));
 	renderer.setSize(window.innerWidth, window.innerHeight);
 	container.appendChild(renderer.domElement);
 
-    var scene = new THREE.Scene();
-
-    var aspect = window.innerWidth / window.innerHeight;
-    camera = new THREE.PerspectiveCamera(60, aspect, 1, 1000);
-    camera.lookAt(scene.position);
-	renderer.render(scene, camera);
-
+    scene = new THREE.Scene();
+    initCamera();
+    initLighting()
+    roomLoader().then(room => {
+        scene.add(room);
+        room.position.set(0, 0, 0); 
+    })
+    controls = new OrbitControls(camera, renderer.domElement)
     render();
 }
 
+function initCamera(){
+    var aspect = window.innerWidth / window.innerHeight;
+    camera = new THREE.PerspectiveCamera(60, aspect, 1, 1000);
+    camera.position.set(1,0,0)
+    camera.lookAt(scene.position);
+}
+
+function initLighting() {
+  // Ambient light
+  const ambientLight = new THREE.AmbientLight(0xffffff, 1.0)
+  scene.add(ambientLight)
+}
+
 function render() {
-    requestAnimationFrame(render)
+    requestAnimationFrame(render);
+    controls.update()
     renderer.render(scene, camera);
 }
 
+
 function onResize() {
     var aspect = window.innerWidth / window.innerHeight;
-
     camera.aspect = aspect;
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
-window.onload = init;
-
+window.onload = main;
 window.addEventListener("resize", onResize, true);
---
+++ roomLoader.js
@@ -0,0 +1,18 @@
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
+export async function roomLoader() {
+    const loader = new GLTFLoader();
+    const path = "models/room/scene.gltf";
+    try {
+        const gltf = await new Promise((resolve, reject) => {
+            loader.load(path, resolve, undefined, reject);
+        })
+
+        const room = gltf.scene;
+
+        return room;
+    } catch(e){
+        console.error("Error loading Room:", e);
+    }
+}
